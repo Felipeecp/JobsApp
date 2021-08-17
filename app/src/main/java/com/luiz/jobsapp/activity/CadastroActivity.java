4e7971330e5a42fffe1f3fb421df5b2936053d30
Felipecp <felipe.engcp@gmail.com>
2021-08-17 14:40:18 -0300

    Adicionando tela de login e de cadastro

--- app/src/main/java/com/luiz/jobsapp/activity/CadastroActivity.java
+++ app/src/main/java/com/luiz/jobsapp/activity/CadastroActivity.java
@@ -1,14 +1,16 @@
-package com.luiz.jobsapp;
+package com.luiz.jobsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.luiz.jobsapp.R;
+
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
     }
 }
