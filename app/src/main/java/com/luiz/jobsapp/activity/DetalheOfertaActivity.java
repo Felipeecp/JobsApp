4c0d06a05695d7e6f1752c0cb06b1c24fde1d629
unknown <clebmachado.em@gmail.com>
2021-08-31 22:30:45 -0300

    Adicionado tela de detalhe e Contratar

--- app/src/main/java/com/luiz/jobsapp/activity/DetalheOfertaActivity.java
+++ app/src/main/java/com/luiz/jobsapp/activity/DetalheOfertaActivity.java
@@ -12,5 +12,7 @@ public class DetalheOfertaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe_oferta);
+
+
     }
 }
