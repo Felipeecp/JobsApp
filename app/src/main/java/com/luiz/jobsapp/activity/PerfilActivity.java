60461a9a458f6c610653ac758d80e6820fba0774
clebemachado <clebmachado.em@gmail.com>
2021-09-07 14:44:31 -0300

    Adicionado toast de alerta para atualizar dados

--- app/src/main/java/com/luiz/jobsapp/activity/PerfilActivity.java
+++ app/src/main/java/com/luiz/jobsapp/activity/PerfilActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -90,7 +91,10 @@ public class PerfilActivity extends AppCompatActivity {
                     txtExperiencias.setText(dadosUser.getExperiencia());
                     txtTempo.setText(dadosUser.getTempo());
                 } else {
-                    System.out.println("Snapshot não existe");
+                    Toast.makeText(getApplicationContext(),
+                            "Atenção, adicione seus dados",
+                            Toast.LENGTH_LONG)
+                            .show();
                 }
             }
 
